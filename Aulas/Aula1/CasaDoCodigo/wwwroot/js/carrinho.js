009818e32a9e56876687a650b9496a26eaa15359
David Alexandre <david.paula.dias@everis.com>
2019-10-02 22:05:24 -0300

    atualização da quantidade

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -0,0 +1,44 @@
+﻿class Carrinho {
+
+    clickImpremento(btn) {
+        let data = this.getData(btn);
+        data.quantidade++;
+        this.postQuantidade(data);
+  
+    }
+
+    clickDecremento(btn) {
+        let data = this.getData(btn);
+        data.quantidade--;
+        this.postQuantidade(data);
+    }
+
+    getData(elemento) {
+        var linhaDoItem = $(elemento).parents('[item-id]')
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtde = $(linhaDoItem).find('input').val();
+        return {
+            Id: itemId,
+            quantidade: novaQtde
+        }
+    }
+
+    postQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updateQuantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        })
+            
+    }
+
+    updateQuantidade(input) {
+        let data = this.getData(input);
+        this.postQuantidade(data);
+
+
+    }
+
+}
+var carrinho = new Carrinho();
