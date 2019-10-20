26c61c6c44663ca5d1f2dce025378e8b1c4f3181
David Alexandre <david.paula.dias@everis.com>
2019-10-19 22:59:59 -0300

    Limpa

--- Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -1,10 +1,5 @@
 ﻿using CasaDoCodigo.Models;
-using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CasaDoCodigo
 {
